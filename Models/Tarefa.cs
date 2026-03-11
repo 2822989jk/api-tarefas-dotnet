a48622b0bc48e3f9c7c35e8feba1e85f129bca23
Vitória C. Oliveira <vickharuno19@gmail.com>
2026-03-11 08:01:23 -0400

    Tarefa.cs
    Adiciona model Tarefa

---
+++ Models/Tarefa.cs
@@ -0,0 +1,11 @@
+namespace ApiTarefas.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Concluida { get; set; }
+    }
+}
